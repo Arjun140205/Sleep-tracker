b9d46251da52fb43f53a9ebc151b3a78348f1735
Arjunbir Singh <arjunbirsingh1699@gmail.com>
2025-07-04 21:47:10 +0530

    fix: enhance Login flow and error feedback messages

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:5000/api/login", {
+      const response = await fetch("http://localhost:5001/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
